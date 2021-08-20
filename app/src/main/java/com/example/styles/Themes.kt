8abccef2090c40681c5f25c52ea7e5ee2d874807
Igor Novikov <igor.novikov@umbrellait.com>
2021-08-20 06:47:35 +0300

    text styles

---
+++ app/src/main/java/com/example/styles/Themes.kt
@@ -0,0 +1,11 @@
+package com.example.styles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Themes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_themes)
+    }
+}
