bd192eb1e51fbfaa5bae2f51e84ddbf8f039dcaf
Igor Novikov <igor.novikov@umbrellait.com>
2021-08-31 13:52:57 +0300

    Spinner works

--- spinner/src/main/java/com/example/spinner/MainActivity.kt
+++ spinner/src/main/java/com/example/spinner/MainActivity.kt
@@ -2,15 +2,40 @@ package com.example.spinner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import com.example.spinner.adapters.HabrAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val spinner: Spinner = findViewById(R.id.my_spinner)
+//        initSimpleSpinner()
+        initSimpleSpinnerWithHabrAdapter()
+    }
+
+    private fun initSimpleSpinnerWithHabrAdapter() {
+        val spinner = findViewById<View>(R.id.simple_spinner_habr_adapter) as Spinner
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.planets_array_short,
+            android.R.layout.simple_spinner_item
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        spinner.adapter = HabrAdapter(
+            adapter,
+            R.layout.first_item,
+            this
+        )
+    }
+
+    private fun initSimpleSpinner() {
+        val spinner: Spinner = findViewById(R.id.simple_spinner)
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter.createFromResource(
             this,
